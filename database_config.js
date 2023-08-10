3895854bf07e4653691a409744209da1cf15b61f
Alexandr Petrenko <alexandrpetrenko04@gmail.com>
2023-08-10 15:38:47 +0300

    game_logic

---
+++ database_config.js
@@ -0,0 +1,27 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+};
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const client = new MongoClient(process.env.DATABASE_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+module.exports = {
+  db: client.db('47Database')
+};
--- server.js
+++ server.js
@@ -1,46 +1,71 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const { db } = require('./database_config');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const CronJob = require('cron').CronJob;
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 
+const READLER = 'READLER';
+const GUESSER = 'GUESSER';
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://hekot77493:nKccvpfYQ5Al2812@cluster1.ippbhts.mongodb.net/?retryWrites=true&w=majority";
+async function startGame(data) {
+  const players = data ? data : await db.collection('players').find({}).toArray();
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+  if(_.get(players, 'length') >= 2) {
+    const gameArray = [];
+    const targetLength = Math.pow(2, Math.floor(Math.log2(players.length)));
+    const arr = users.slice(0, targetLength).sort(() => 0.5 - Math.random());
+    const readlers = arr.slice(0, arr.length / 2);
+    const guessers = arr.slice(arr.length / 2, arr.length);
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    // await client.db('47Database').createCollection('users');
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    // console.log(await client.db('47Database').collection("players").findOne({}))
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    for(let i = 0; i < arr.length / 2; i++) {
+      Object.assign(readlers[i], { type: READLER, answer: null });
+      Object.assign(guessers[i], { type: GUESSER, answer: null });
+      gameArray.push({readler: readlers[i], guesser: guessers[i], roomId});
+      
+      io.emit('new-user', readlers[i]);
+      io.emit('new-user', guessers[i]);
+    }
+
+    io.emit('gameArray', gameArray);
+
+  } else {
+    io.emit('Start game failed', 'Игра не началась, недостаточное колличество игроков!')
   }
 }
-run().catch(console.dir);
+/// post anwser
 
-const db = client.db('47Database');
-const users = db.collection("users");
+app.post('/answer', async (req, res) => {
+  const {
+    data
+  } = req.body;
+  const roundComplete = data.every(({readler, guesser}) => (readler.answer && guesser.answer));
+  if(roundComplete) {
+    const filteredData = [];
 
+    data.forEach(({readler, guesser}) => {
+      if(readler.answer == guesser.answer) {
+        filteredData.push({...guesser});
+      } else {
+        filteredData.push({...readler});
+      }
+      io.sockets.sockets[readler.socketId].leave(readler.roomId);
+      io.sockets.sockets[guesser.socketId].leave(guesser.roomId);
+    });
+
+    if(_.get(filteredData, 'length') > 1) {
+        await startGame(filteredData);
+    } else {
+      await db.collection('players').deleteMany({});
+      
+      await db.collection('winners').insertOne({...filteredData.slice(0, 1)});
+
+      io.emit('Game finished', { winner: {...filteredData.slice(0, 1)} });
+    }
+  }
+})
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -104,99 +129,64 @@ app.put('/user', async (req, res) => {
   }
 });
 
-// get all data const userDataA = await users.find().toArray();
 
 app.get('/', (req, res) => {
-  console.log('--------------------------', rooms)
   res.render('index', { rooms: rooms })
 })
 
-app.post('/room', (req, res) => {
-  const targetLength = Math.pow(2, Math.floor(Math.log2(users.length)));
-  let arr = users.slice(0, targetLength);
-  arr = arr.sort(() => 0.5 - Math.random());
-  const roomPairs = [];
-  const readlers = arr.slice(0, arr.length / 2)
-  const answermans = arr.slice(arr.length / 2, arr.length)
-
-  for(let i = 0; i < arr.length / 2; i++) {
-    readlers[i].type = 'Readler';
-    answermans[i].type = 'AnswerMan';
-    roomPairs.push({readler: readlers[i], answerman: answermans[i], roomId: i + 1})
-  }
-
-
-  roomPairs.forEach(({readler, answerman, roomId}) => {
-    rooms[roomId] = { users: {} }
-    console.log('111111111111111111111111', roomId)
-    io.emit('room-created', roomId)
-    io.emit('new-user', roomId, readler)
-    io.emit('new-user', roomId, answerman)
-  })
-  console.log('22222222222222222222222222', rooms)
-  // if (rooms[req.body.room] != null) {
-  //   return res.redirect('/')
-  // }
-  // rooms[req.body.room] = { users: {} }
-  // res.redirect(req.body.room)
-  // // Send message that new room was created
-  // io.emit('room-created', req.body.room)
-})
-
-app.post('/create_user', (req, res) => {
-  users.push({
-    name: "ALEX",
-    id: 3,
-    type: 'Readler'
-  })
-  console.log('users', users)
-  io.emit('user-created')
-})
-
-app.get('/get_user', (req, res) => {
-  console.log('/////////////////', users.find(el=> el.id = 2))
-  return users.find(el=> el.id = 2);
-})
-
-app.get('/:room', (req, res) => {
-  if (rooms[req.params.room] == null) {
-    return res.redirect('/')
-  }
-  res.render('room', { roomName: req.params.room })
+app.post('/participate', async (req, res) => {
+  const {
+    _id,
+    login,
+  } = req.body
+  await db.collection('players').insertOne({userId: _id, name: login});
 })
 
 server.listen(10000)
 
-io.on('connection', socket => {
-  const job = new CronJob('0 * * * * *',
-  function() {
-      console.log('You will see this message every second');
-  },
-  null,
-  true,
-  'Europe/Riga'
-)
-job.start()
-  socket.on('new-user', (room, name) => {
-    socket.join(room)
-    rooms[room].users[socket.id] = name
-    console.log('11111111111111111111111111111111111', socket.id, name, rooms)
-    socket.to(room).broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
-      delete rooms[room].users[socket.id]
+io.on('connection', async socket => {
+    const {
+        gameStartHour
+    } = await db.collection('timer_settings').findOne({});
+
+    const job = new CronJob(`0 0 ${gameStartHour} * * *`,
+        async () => {
+                await startGame();
+            },
+            null,
+            true,
+            'Europe/Riga'
+    )
+    
+    job.start();
+
+    socket.on('new-user', (player) => {
+        const {
+            name,
+            type,
+            roomId
+        } = player;
+
+        socket.join(roomId);
+        player.socketId = socket.id;
+
+        socket.to(roomId).broadcast.emit('user-connected', name);
     })
-  })
+
+    // socket.on('send-chat-message', (room, message) => {
+    //   socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+    // })
+    socket.on('disconnect', () => {
+        getUserRooms(socket).forEach(room => {
+            socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+            delete rooms[room].users[socket.id]
+        })
+    })
 })
 
 function getUserRooms(socket) {
-  return Object.entries(rooms).reduce((names, [name, room]) => {
-    if (room.users[socket.id] != null) names.push(name)
-    return names
-  }, [])
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if (room.users[socket.id] != null) names.push(name)
+        return names
+    }, [])
 }
