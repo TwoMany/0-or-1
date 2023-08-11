b9abb359476a71eb390da5d3df22785f619ced5f
Marian Fedkovych <m.fed@intobi.com>
2023-08-11 11:51:06 +0300

    upd front

--- client/src/pages/MainPage/index.js
+++ client/src/pages/MainPage/index.js
@@ -2,7 +2,7 @@ import { Button, Space } from "antd";
 import Countdown from "react-countdown";
 import { LoginForm } from "./LoginForm";
 import { postData } from "../../tools";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { socket } from "../../socket";
 
 import { get } from "lodash";
@@ -10,7 +10,7 @@ import { get } from "lodash";
 export const MainPage = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [user, setUser] = useState(localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : undefined);
-  const [rooms, setRooms] = useState([]);
+  const [players, setPlayers] = useState([]);
 
   useEffect(() => {
     user && localStorage.setItem("user", JSON.stringify(user));
@@ -19,36 +19,43 @@ export const MainPage = () => {
   useEffect(() => {
     function onConnect(value) {
       setIsConnected(true);
-      console.log(value);
+      // console.log(value);
     }
 
     function onDisconnect(value) {
       setIsConnected(false);
-      console.log(value);
+      // console.log(value);
     }
 
-    function onFooEvent(value) {
+    function onPlayersChange(value) {
       console.log(value);
+      setPlayers(value);
     }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('gameArray', onFooEvent);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("players", onPlayersChange);
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('gameArray', onFooEvent);
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("players", onPlayersChange);
     };
   }, []);
 
-  const room = rooms.find(({ id }) => id === user.id);
 
-  const handleSendAnswer = (answer) => {
-    postData("/answer", { answer }).then((data) => {
-      console.log(data);
-    });
-  };
+  const handleSendAnswer = useCallback(
+    (answer) => {
+      postData("/answer", { answer, userId: user._id }).then((data) => {
+        console.log(data);
+      });
+    },
+    [user]
+  );
+
+  const playerIndex = players.findIndex(({userId}) => user._id === userId);
+
+  console.log('room', user, players);
 
   return (
     <Space direction="vertical" align="center" style={{ width: "100%" }}>
@@ -56,29 +63,30 @@ export const MainPage = () => {
       <Countdown date={new Date().setHours(21, 0, 0, 0)} />
 
       {user ? (
-        <Space>
-          {get(user, "participate") ? (
+        <Space direction="vertical" align="center">
+          <div>{user.login}</div>
+          {playerIndex >= 0 ? (
             <>
               <div>
                 <Space>
-                  {user.login} {user.answer}
+                  {get(players, `[${playerIndex}].name`)} {get(players, `[${playerIndex}].answer`)}
                 </Space>
                 <Space>
-                  {get(room, "oponent.name")} {get(room, "oponent.answer")}
+                  {get(players, `[${playerIndex + 1}].name`)} {get(players, `[${playerIndex + 1}].answer`)}
                 </Space>
               </div>
-              <Space.Compact block>
-                <Button onClick={() => handleSendAnswer(0)}>0</Button>
-                <Button onClick={() => handleSendAnswer(1)}>1</Button>
-              </Space.Compact>
+              {!get(players, `[${playerIndex + 1}].answer`) && (
+                <Space.Compact block>
+                  <Button onClick={() => handleSendAnswer(0)}>0</Button>
+                  <Button onClick={() => handleSendAnswer(1)}>1</Button>
+                </Space.Compact>
+              )}
             </>
           ) : (
             <Button
               onClick={() => {
                 postData("/participate", user).then((data) => {
-                  console.log(data);
-
-                  setUser({ ...user, participate: true });
+                  
                 });
               }}
             >
