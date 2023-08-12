3c4ded6576ad5ca02ceb37f50e00cb0e8e96eb83
Marian Fedkovych <m.fed@intobi.com>
2023-08-12 19:53:00 +0300

    10000 to 443

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import * as io from 'socket.io-client'
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.REACT_APP_ENVIRONMENT === 'production' ? '191.101.2.121:10000' : 'http://localhost:10000';
+const URL = process.env.REACT_APP_ENVIRONMENT === 'production' ? '191.101.2.121:443' : 'http://localhost:443';
 console.log(process.env, URL)
 
 export const socket = io(URL);
