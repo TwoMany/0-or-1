6469e2be505b475bc60d2f7e5bc9d6d1a0ee7d11
Alexandr Petrenko <alexandrpetrenko04@gmail.com>
2023-08-18 10:08:51 +0300

    fix

--- server.js
+++ server.js
@@ -82,7 +82,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://illusiumgame.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
